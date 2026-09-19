f048e09c15d564a1ed55f34a6554784952c0fa53
Samuel Bachmann <samuel.bachmann@gmail.com>
2026-09-19 20:53:18 +0200

    improve time frame performance
    ListTimeFrame append is 0.35 M/s → 31 M/s (~89×), in line with Vector (28 M/s).

--- src/rqt_multiplot/CurveDataListTimeFrame.cpp
+++ src/rqt_multiplot/CurveDataListTimeFrame.cpp
@@ -16,7 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
  ******************************************************************************/
 
-#include <QVector>
+#include <cmath>
 
 #include "rqt_multiplot/CurveDataListTimeFrame.h"
 
@@ -35,11 +35,11 @@ CurveDataListTimeFrame::~CurveDataListTimeFrame() = default;
 /*****************************************************************************/
 
 size_t CurveDataListTimeFrame::getNumPoints() const {
-  return points_.count();
+  return points_.size();
 }
 
 QPointF CurveDataListTimeFrame::getPoint(size_t index) const {
-  return points_[static_cast<int>(index)];
+  return points_[index];
 }
 
 BoundingRectangle CurveDataListTimeFrame::getBounds() const {
@@ -50,47 +50,53 @@ BoundingRectangle CurveDataListTimeFrame::getBounds() const {
 /* Methods                                                                   */
 /*****************************************************************************/
 
-void CurveDataListTimeFrame::appendPoint(const QPointF& point) {
-  points_.append(point);
-
-  double timeCutoff = point.x() - timeFrameLength_;
-
-  //  QMutableListIterator<QPointF> iterator(points_);
-  //  while (iterator.hasNext()) {
-  //    if (iterator.next().x() < timeCutoff)
-  //      iterator.remove();
-  //    else
-  //      break;
-  //  }
-
-  QList<QPointF>::iterator it = points_.begin();
-  while (it != points_.end()) {
-    if ((*it).x() < timeCutoff) {
-      it = points_.erase(it);
-    } else {
-      break;
+void CurveDataListTimeFrame::dropExpiredPoints(double timeCutoff) {
+  while (!points_.empty() && points_.front().x() < timeCutoff) {
+    const double y = points_.front().y();
+    if (std::isfinite(y)) {
+      const auto yIt = yValues_.find(y);
+      if (yIt != yValues_.end()) {
+        yValues_.erase(yIt);
+      }
     }
+    points_.pop_front();
   }
+}
 
-  auto min_max_x = std::minmax_element(points_.begin(), points_.end(), [](const QPointF& a, const QPointF& b) { return a.x() < b.x(); });
+void CurveDataListTimeFrame::syncBounds() {
+  if (points_.empty()) {
+    bounds_.clear();
+    return;
+  }
+  const double yMin = yValues_.empty() ? points_.front().y() : *yValues_.begin();
+  const double yMax = yValues_.empty() ? yMin : *yValues_.rbegin();
+  bounds_.setMinimum(QPointF(points_.front().x(), yMin));
+  bounds_.setMaximum(QPointF(points_.back().x(), yMax));
+}
 
-  auto min_max_y = std::minmax_element(points_.begin(), points_.end(), [](const QPointF& a, const QPointF& b) { return a.y() < b.y(); });
-
-  bounds_.setMinimum(QPointF(min_max_x.first->x(), min_max_y.first->y()));
-  bounds_.setMaximum(QPointF(min_max_x.second->x(), min_max_y.second->y()));
+void CurveDataListTimeFrame::appendPoint(const QPointF& point) {
+  dropExpiredPoints(point.x() - timeFrameLength_);
+  points_.push_back(point);
+  if (std::isfinite(point.y())) {
+    yValues_.insert(point.y());
+  }
+  syncBounds();
 }
 
 void CurveDataListTimeFrame::replacePoints(const QVector<QPointF>& points) {
-  points_.clear();
-  bounds_.clear();
-  for (const auto& point : points) {
-    points_.append(point);
-    bounds_ += point;
+  points_.assign(points.begin(), points.end());
+  yValues_.clear();
+  for (const auto& point : points_) {
+    if (std::isfinite(point.y())) {
+      yValues_.insert(point.y());
+    }
   }
+  syncBounds();
 }
 
 void CurveDataListTimeFrame::clearPoints() {
   points_.clear();
+  yValues_.clear();
   bounds_.clear();
 }
 
--- test/CurveDataPolicyTest.cpp
+++ test/CurveDataPolicyTest.cpp
@@ -1,3 +1,5 @@
+#include <limits>
+
 #include <QPointF>
 #include <QVector>
 
@@ -29,6 +31,102 @@ TEST(CurveDataListTimeFrame, appendPointDropsPointsOutsideWindow) {
   ASSERT_EQ(data.getNumPoints(), 2u);
   EXPECT_DOUBLE_EQ(data.getPoint(0).x(), 5.0);
   EXPECT_DOUBLE_EQ(data.getPoint(1).x(), 12.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointXBoundsAreOldestAndNewest) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(0.0, 1.0));
+  data.appendPoint(QPointF(5.0, 2.0));
+  data.appendPoint(QPointF(12.0, 3.0));
+
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().x(), 5.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().x(), 12.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointRescansYWhenDroppedPointWasMaximum) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(0.0, 10.0));
+  data.appendPoint(QPointF(5.0, 2.0));
+  data.appendPoint(QPointF(6.0, 3.0));
+  data.appendPoint(QPointF(12.0, 4.0));
+
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().y(), 2.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().y(), 4.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointRescansYWhenDroppedPointWasMinimum) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(0.0, -4.0));
+  data.appendPoint(QPointF(5.0, 2.0));
+  data.appendPoint(QPointF(6.0, 3.0));
+  data.appendPoint(QPointF(12.0, 1.0));
+
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().y(), 1.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().y(), 3.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointKeepsYBoundsWhenDroppedPointIsInterior) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(0.0, 5.0));
+  data.appendPoint(QPointF(5.0, 1.0));
+  data.appendPoint(QPointF(6.0, 9.0));
+  data.appendPoint(QPointF(12.0, 4.0));
+
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().y(), 1.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().y(), 9.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointRescansYWhenMultipleExtremaExpire) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(0.0, 1.0));
+  data.appendPoint(QPointF(1.0, 8.0));
+  data.appendPoint(QPointF(2.0, 4.0));
+  data.appendPoint(QPointF(20.0, 3.0));
+
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().x(), 20.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().x(), 20.0);
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().y(), 3.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().y(), 3.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointBoundsTrackMonotonicRamp) {
+  CurveDataListTimeFrame data(10.0);
+  for (int i = 0; i < 20; ++i) {
+    data.appendPoint(QPointF(static_cast<double>(i), static_cast<double>(i)));
+  }
+
+  ASSERT_EQ(data.getNumPoints(), 11u);
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().x(), 9.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().x(), 19.0);
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().y(), 9.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().y(), 19.0);
+}
+
+TEST(CurveDataListTimeFrame, appendPointIgnoresNanYForBounds) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(0.0, 1.0));
+  data.appendPoint(QPointF(1.0, std::numeric_limits<double>::quiet_NaN()));
+  data.appendPoint(QPointF(2.0, 3.0));
+
+  ASSERT_EQ(data.getNumPoints(), 3u);
+  const auto bounds = data.getBounds();
+  EXPECT_DOUBLE_EQ(bounds.getMinimum().y(), 1.0);
+  EXPECT_DOUBLE_EQ(bounds.getMaximum().y(), 3.0);
+}
+
+TEST(CurveDataListTimeFrame, clearPointsInvalidatesBounds) {
+  CurveDataListTimeFrame data(10.0);
+  data.appendPoint(QPointF(1.0, 2.0));
+  data.clearPoints();
+
+  EXPECT_TRUE(data.isEmpty());
+  EXPECT_FALSE(data.getBounds().isValid());
 }
 
 TEST(CurveDataListTimeFrame, replacePointsKeepsFullIndexSnapshot) {
